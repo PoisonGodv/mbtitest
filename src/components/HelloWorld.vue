8ffa5093047b11d4b14d28c525e164fedd847b75
poisongodv <951448078@qq.com>
2024-10-09 22:32:34 +0800

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div v-if="questions.length > 0">
+    <div v-for="question in questions" :key="question.question">
+      <p>{{ question.question }}</p>
+      <el-radio-group v-model="answers[question.question]">
+        <el-radio v-if="question.options && question.options.A" :label="question.options.A">{{ question.options.A }}</el-radio>
+        <el-radio v-if="question.options && question.options.B" :label="question.options.B">{{ question.options.B }}</el-radio>
+      </el-radio-group>
+    </div>
+    <el-button @click="submit">提交表单</el-button>
+    <p>{{analyze}}</p>
+  </div>
+  <div v-else>
+    <p>正在加载数据...</p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from "axios";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  data() {
+    return {
+      questions: [],
+      answers: {},
+      analyze:"",
+    };
+  },
+  mounted() {
+    this.btnclicked();
+  },
+  methods: {
+    cleanMarkdown(text) {
+      // 如果这里的所谓 Markdown 标记只是特定字符，可以直接替换
+      return text.replace(/```json/g, '').replace(/```/g, '');
+    },
+    submit() {
+
+      axios.post('http://localhost:8080/submitAnswers', this.answers)
+          .then(response => {
+            console.log(response.data.message);
+            this.analyze = response.data.message;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    },
+    btnclicked: function () {
+      axios.get('http://localhost:8080/getMbit')
+          .then(response => {
+            // 处理请求成功的结果
+
+
+            this.questions = JSON.parse('{' + this.cleanMarkdown(response.data.question)+ '}').questions;
+            console.log(this.questions.questions);
+          })
+          .catch(error => {
+            // 处理请求失败的情况
+            console.error(error);
+          });
+    }
+  }
+};
+</script>
